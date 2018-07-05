83527fc25fda7dcec7513c2a2fe4b2e05e3888e1
Jenkins <jenkins@opennodecloud.com>
2018-07-05 12:28:55 +0000

    Preparing new release: 0.2.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [
 
 setup(
     name='waldur-rijkscloud',
-    version='0.2.4',
+    version='0.2.5',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='https://waldur.com',
